9c7e9eb293d22c76b554c76ec5db82cc2371f485
millerpipelineq <quinn.millsop@millerpipeline.com>
2018-07-27 14:25:22 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/17TyVdQ7by6V-Bb4PXSRG3Uh2xq6OzAQIEDDt6V_nA3s/edit#gid=0';
