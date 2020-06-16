e97550ad007d2dc0563f06a6a93ca931a4cac2e3
millerpipelineq <quinn.millsop@millerpipeline.com>
2020-06-16 14:06:46 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -126,9 +126,9 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' + point['Description1'] +
+          .bindPopup("<b>" + point['Name'] + '</b><br>' + point['Description'] +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description']);
+          point['Description1']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
