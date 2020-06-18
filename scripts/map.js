0bbd7f23384b55b181493be494c4c4e50033a141
millerpipelineq <quinn.millsop@millerpipeline.com>
2020-06-18 08:04:50 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -126,7 +126,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' + point['Description'] + '</b><br>' +  point['Footage'] + '</b><br>
+          .bindPopup("<b>" + point['Name'] + '</b><br>' + point['Description'] + '</b><br>' +  point['Footage'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
           point['Description1'] + '</b><br>' + point['Locate']);
 
